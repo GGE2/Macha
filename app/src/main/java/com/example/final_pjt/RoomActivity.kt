0d5cb149941da3d2329731d1b70f94c8de569b43
kosdjs39 <kosdjs39@gmail.com>
2022-11-17 14:44:19 +0900

    game end dialog initial commit

--- app/src/main/java/com/example/final_pjt/RoomActivity.kt
+++ app/src/main/java/com/example/final_pjt/RoomActivity.kt
@@ -1,8 +1,11 @@
 package com.example.final_pjt
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.final_pjt.databinding.ActivityRoomBinding
+import com.example.final_pjt.databinding.DialogGameEndBinding
 
 class RoomActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRoomBinding
@@ -10,5 +13,11 @@ class RoomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.roomStartButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val dialogBinding = DialogGameEndBinding.inflate(builder.create().layoutInflater)
+            builder.setView(dialogBinding.root)
+            builder.setPositiveButton("확인") { dialog, _ -> dialog?.cancel() }.show()
+        }
     }
 }

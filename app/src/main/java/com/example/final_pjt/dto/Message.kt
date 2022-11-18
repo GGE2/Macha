ee9059e52a62625650e9e66a31dd5e0f89457631
kosdjs <kosdjs39@gmail.com>
2022-11-18 22:18:37 +0900

    채팅 관련 dto 수정

--- app/src/main/java/com/example/final_pjt/dto/Message.kt
+++ app/src/main/java/com/example/final_pjt/dto/Message.kt
@@ -3,12 +3,12 @@ package com.example.final_pjt.dto
 import com.google.gson.annotations.SerializedName
 
 class Message (
-    @SerializedName(value = "writer")
-    var userToken : String,
+    @SerializedName(value = "user")
+    var user : User,
     @SerializedName(value = "message")
     var text : String,
         ){
     override fun toString(): String {
-        return "Message(userToken='$userToken', text='$text')"
+        return "Message(user=$user, text='$text')"
     }
 }
--- app/src/main/java/com/example/final_pjt/dto/User.kt
+++ app/src/main/java/com/example/final_pjt/dto/User.kt
@@ -4,11 +4,12 @@ data class User(
     var userToken:String,
     var nickname : String,
     var profileImg : String,
-    var isOnline : Int = 1
+    var isOnline : Int = 1,
+    var userId : Int = -1
 
 
 ) : java.io.Serializable{
     override fun toString(): String {
-        return "User(userToken='$userToken', nickname='$nickname', profileImg='$profileImg', isOnline=$isOnline)"
+        return "User(userToken='$userToken', nickname='$nickname', profileImg='$profileImg', isOnline=$isOnline, userId=$userId)"
     }
 }

274c6d4f947a6ba19d84f89e5d9f5da993dc1954
kosdjs39 <kosdjs39@gmail.com>
2022-11-21 09:12:58 +0900

    서버 ip 변경

--- app/src/main/java/com/example/final_pjt/util/ApllicationClass.kt
+++ app/src/main/java/com/example/final_pjt/util/ApllicationClass.kt
@@ -9,7 +9,7 @@ class ApplicationClass : Application(){
 
 
     //url must be end with "/"
-    val BOARD_URL   = "http://13.209.5.95:8080/"
+    val BOARD_URL   = "http://54.180.24.155:8080/"
 
     companion object {
         // 전역변수 문법을 통해 Retrofit 인스턴스를 앱 실행 시 1번만 생성하여 사용 (싱글톤 객체)
